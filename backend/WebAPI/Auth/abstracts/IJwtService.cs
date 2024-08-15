49bb0199911836fbd9410958387eaa7fd9442649
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-08-15 19:03:16 +0300

    Create IJwtService.cs

---
+++ backend/WebAPI/Auth/abstracts/IJwtService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace WebAPI.Auth.abstracts
+{
+    public interface IJwtService
+    {
+        string GenerateToken(string userId, string email, string role);
+        string GenerateToken(IEnumerable<Claim> claims);
+        (bool isValid, ClaimsPrincipal claimsPrincipal) ValidateToken(string token);
+        IDictionary<string, string> DecodeToken(string token);
+        bool IsTokenExpired(string token);
+        string RefreshToken(string token);
+    }
+}
