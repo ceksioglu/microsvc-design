fc6848fe37902620c57d5540cfe84bb02c52de38
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-08-15 22:12:59 +0300

    Update IRabbitMQService.cs

--- backend/WebAPI/Packages/RabbitMQ/abstracts/IRabbitMQService.cs
+++ backend/WebAPI/Packages/RabbitMQ/abstracts/IRabbitMQService.cs
@@ -5,7 +5,7 @@ namespace WebAPI.Packages.RabbitMQ.abstracts
 {
     public interface IRabbitMQService
     {
-        void PublishMessage<T>(string exchange, string routingKey, T message, string exchangeType = "direct");
+        Task PublishMessage<T>(string exchange, string routingKey, T message, string exchangeType = "direct");
         void ConsumeMessage<T>(string queueName, Func<T, Task> messageHandler);
         void BindQueue(string queueName, string exchange, string routingKey);
         void DeclareExchange(string exchangeName, string exchangeType = "direct");
