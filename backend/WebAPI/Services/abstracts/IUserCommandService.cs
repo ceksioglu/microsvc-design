a148d6b840565a30f3035393a2f49f905888d71e
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-08-22 22:17:40 +0300

    Create IUserCommandService.cs

---
+++ backend/WebAPI/Services/abstracts/IUserCommandService.cs
@@ -0,0 +1,12 @@
+using WebAPI.DTO;
+
+namespace WebAPI.Services.abstracts;
+
+public interface IUserCommandService
+{
+    Task<UserResponseDto> RegisterUserAsync(UserCreateDto userDto);
+    Task<UserResponseDto> UpdateUserAsync(int id, UserUpdateDto userDto);
+    Task<bool> DeleteUserAsync(int id);
+    Task<string> AuthenticateUserAsync(string email, string password);
+    Task<bool> ChangePasswordAsync(int userId, string currentPassword, string newPassword);
+}
